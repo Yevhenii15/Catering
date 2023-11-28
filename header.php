57d9653952ead73362ac24b1670fd6e935921861
Yevhenii <yevmot01@easv365.dk>
2023-11-28 22:26:02 +0100

    add logo changed position and size of all elements as it should look like, changed structure, for homepage you need just add wave for mobile version and icons of some and all terms

--- header.php
+++ header.php
@@ -7,12 +7,15 @@
     <?php wp_head() ?>
 </head>
 <body>
-    <header>
+    <header>          
+       <a class="logo" href="<?php echo get_site_url(); ?>">
+            <img src="http://cateringbbq.local/wp-content/themes/catering/Catering/assets/Logo.svg" alt="">
+        </a>
         <nav>
             <ul>
                 <a href="<?php echo get_site_url(); ?>/shop" class="menu ">
                     <img class="top" src="http://cateringbbq.local/wp-content/themes/catering/Catering/assets/Menu-Top.svg" alt="">
-                    <p>Menu</p>
+                        <p>Menu</p>
                     <img class="bottom" src="http://cateringbbq.local/wp-content/themes/catering/Catering/assets/Menu-Bottom.svg" alt="">
                 </a>
                 <li class="btn">
--- homepage.php
+++ homepage.php
@@ -23,26 +23,8 @@ $welcome_additional_text = get_field('welcome_additional_text');
 <?php get_footer(); ?>
 
 <style scoped>
-/* General Styles */
-
-
-.rb {
-  border: 1px solid red;
-}
 
 /* Background Styles */
-body {
-  content: "";
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-image: url('http://cateringbbq.local/wp-content/uploads/2023/11/bg-grills.png');
-  background-size: cover;
-  background-position: center bottom;
-  background-attachment: fixed;
-}
 .svg-background {
   position: fixed;
   top: 0;
@@ -55,25 +37,29 @@ body {
 /* Header Styles */
 .titles {
   position: absolute;
-  top: 304.7L1440em;
-  padding-left:5%;
+  top: 9em;
   line-height: 1.5;
   max-height: 100vh;
-  width: 100vw;
-  font-size: 1.5em;
   display: flex;
   flex-direction: column;
   color: #fff;
   z-index: 1;
+  margin: 1% 10%;
+}
+.titles h2 {
+  letter-spacing: 0.1em;
+  line-height: 1em;
+  font-size: 5em;
+
+}
+.titles h1 {
+  font-family: "itc-avant-garde-gothic-pro", sans-serif;
+  font-weight: 500;
+  font-size: 2.5em;
+  display: flex;
+  justify-content: end;
 }
 
-.top-section {
-  display: flex;
-  flex-wrap: wrap; /* Allow items to wrap onto multiple lines */
-  justify-content: space-between;
-  align-items: flex-start;
-  padding: 2% 5%;
-}
 
 /* Navigation Styles */
 nav {
@@ -106,39 +92,38 @@ a.menu {
 
 a.menu img{
   display: block;
-  width: 70%;
-  z-index: 10;
-  
+  width: 50%;
+}
+.menu p {
+  background-color: var(--button-bg);
+  padding: .1em  1.8em; /* Adjust as needed */
+  margin: 0 0.2em;
+  border: 1px solid var(--gray-shade);
+  border-radius: 20px;
+  backdrop-filter: blur(10px);
+  font-size: 1.5em; /* Adjust as needed */
+  text-align: center; /* Center the text within the button */
 }
 h1,
 h2 {
   position: relative;
-  background-color: none;
   display: flex;
   flex-direction: row;
-  margin: 1% 5%;
-}
-
-h1 {
-  font-size: 2em;
-  padding-left: 5%;
-}
-
-h2 {
-  font-size: 3em;
+  margin: 0;
 }
 
 h1::before {
-  content: "On-Site";
+  content: "On-Site ";
   background-color: none;
   display: block;
+  margin-right: .3em;
 }
 
 a.menu {
   padding-bottom: 2em;
   position: absolute;
   top: 10%;
-  right: 5%;
+  right: 0;
   z-index: 3;
 }
 
@@ -174,21 +159,10 @@ li {
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
   white-space: nowrap;
+  font-size: 0.7em; 
   height: 1.5em;
-  width: 32vw;
-}
-
-.menu p {
-  background-color: var(--button-bg);
-  padding: .1em  1.8em; /* Adjust as needed */
-  margin: 0 0.2em;
-  border: 1px solid var(--gray-shade);
-  border-radius: 20px;
-  backdrop-filter: blur(10px);
-  font-size: 2em; /* Adjust as needed */
-  text-align: center; /* Center the text within the button */
+  padding: 0.1em 2em;
 }
 
 
@@ -199,17 +173,6 @@ li {
     justify-content: center;
     width: 100% ;
   }
-
-  h1 {
-    font-size: 2em;
-
-  }
-
-  h2 {
-    font-size: 3em;
-    /* Center the shop-link horizontally */
-
-  }
   nav{
     margin-top: 0;
     height: 100vh;
@@ -218,12 +181,17 @@ li {
   }
   .titles {
     position: absolute;
-    top: 10%;
+    top: 20%;
     left:0;
     font-size: 1em;
     
   }
-
+  .titles h2 {
+  font-size: 2.5em;
+  }
+  .titles h1 {
+    font-size: 1.5em;
+  }
   nav a{
     margin: 0 40%
   }
@@ -234,12 +202,14 @@ li {
   position: static
   }
  .menu p{
-  padding: 0 50%;
+  padding: 0 70%;
   margin: 0 0.2em;
+  font-size: 1.4em ;
   }
- a.menu img{
-  width: 100%;
- } 
+  a.menu img{
+    width: 180%;
+  } 
+
   .btn {
     font-size: 1.4em;
   }
