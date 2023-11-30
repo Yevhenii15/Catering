fc0f72f3368785c12dfa09bae06d439eb77c8d51
KianMiridoozini <114469799+KianMiridoozini@users.noreply.github.com>
2023-11-30 13:22:55 +0100

    about-us

--- about-us.php
+++ about-us.php
@@ -6,54 +6,72 @@
 
 get_header();
 // Retrieve ACF fields for the about-us page
-$img1 = get_field('img1');
-$text_about_catering = get_field('text_about_catering');
-$img2 = get_field('img2');
+$about_us_top_img = get_field('about_us_top_img');
+$about_us_top_text = get_field('about_us_top_text');
+$about_us_top_text_2 = get_field('about_us_top_text_2');
+$history_title = get_field('history_title');
+$text_about_history = get_field('text_about_history');
+$text_about_competition_title = get_field('text_about_competition_title');
 $text_about_competition = get_field('text_about_competition');
+$img2 = get_field('img2');
+$family_title = get_field('family_title');
+$family_text = get_field('family_text');
+$join_us_title = get_field('join_us_title');
+$join_us_text = get_field('join_us_text');
 $img3 = get_field('img3');
-$img4 = get_field('img4');
-$img5 = get_field('img5');
-$facebook = get_field('facebook');
-$instagram = get_field('instagram');
-$img6 = get_field('img6');
+$contact_us_title = get_field('contact_us_title');
+$contact_us_text = get_field('contact_us_text');
+
 ?>
+    <img class="top-bg-nav"src="<?php echo esc_url($about_us_top_img['url']); ?>" alt="about-us-top-img" >
 
-<section class="catering">
-    <div class="catering-img">
-        <img src="<?php echo esc_url($img1['url']); ?>"   alt="<?php echo $img1['alt']?>">
-    </div>
-    <div class="catering-text">
-        <p><?php echo esc_html($text_about_catering); ?></p>
-    </div>
-</section>
+<div class="about-us flex-col" >
 
-<section class="competition">
-    <div class="competition-img">
-        <img src="<?php echo esc_url($img2['url']); ?>" alt="<?php echo $img2['alt']?>">
-    </div>
-    <div class="competition-text">
-        <p><?php echo esc_html($text_about_competition); ?></p>
-    </div>
-    <div class="competition-img">
-        <img src="<?php echo esc_url($img3['url']); ?>" alt="<?php echo $img3['alt']?>">
-        <img src="<?php echo esc_url($img4['url']); ?>" alt="<?php echo $img4['alt']?>">
-        <img src="<?php echo esc_url($img5['url']); ?>" alt="<?php echo $img5['alt']?>">
-    </div>
-</section>
+        <div class="about-us-top-text flex-col">
+            <p><span class="top-text-one"><?php echo esc_html($about_us_top_text); ?></span><br><span class="top-text-two"><?php echo esc_html($about_us_top_text_2); ?></span></p>
+        </div>
 
-<section class="social">
-    <button class="facebook">
-        <a href="<?php echo esc_url($facebook); ?>" target="_blank">Facebook</a>
-    </button>
-    <button class="instagram">
-        <a href="<?php echo esc_url($instagram); ?>" target="_blank">Instagram</a>
-    </button>
-</section>
+    <section class="history">
+        <div class="history-text flex-col">
+            <h3><?php echo esc_html($history_title); ?></h3>
+            <p><?php echo esc_html($text_about_history); ?></p>
+        </div>
+    </section>
 
-<section class="family">
-    <h3>Meet the family</h3>
-    <div class="family-img">
-        <img src="<?php echo esc_url($img6['url']); ?>" alt="<?php echo $img6['alt']?>">
-    </div>
-</section>
+    <section class="competition flex-row">
+        <div class="competition-text flex-col">
+            <h3><?php echo esc_html($text_about_competition_title); ?></h3>
+            <p><?php echo esc_html($text_about_competition); ?></p>
+        </div>
+        <div class="competition-img">
+            <img src="<?php echo esc_url($img2['url']); ?>" alt="about-us-competition-img" id="img">
+        </div>
+    </section>
+
+    <section class="family flex-row">
+        <div class="family-text flex-col">
+            <h3><?php echo esc_html($family_title); ?></h3>
+            <p><?php echo esc_html($family_text); ?></p>
+        </div>
+    </section>
+
+    <section class="join-us flex-row">
+        <div class="join-us-text flex-col">
+            <h3><?php echo esc_html($join_us_title); ?></h3>
+            <p><?php echo esc_html($join_us_text); ?></p>
+        </div>
+        <div class="join-us-img">
+            <img src="<?php echo esc_url($img3['url']); ?>" alt="about-us-join-us-img" id="img">
+        </div>
+    </section>
+
+    <section class="contact-us">
+        <div class="contact-us-text flex-col">
+            <h3><?php echo esc_html($contact_us_title); ?></h3>
+            <p><?php echo esc_html($contact_us_text); ?></p>
+        </div>
+    </section>
+
+</div>
+
 <?php get_footer(); ?>
