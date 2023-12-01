7a50c361b6e1617e197049c825de85745abfc76f
KianMiridoozini <114469799+KianMiridoozini@users.noreply.github.com>
2023-12-01 12:59:17 +0100

    Yev needs backend. updated cf and images in back-end

--- about-us.php
+++ about-us.php
@@ -11,14 +11,16 @@ $about_us_top_text = get_field('about_us_top_text');
 $about_us_top_text_2 = get_field('about_us_top_text_2');
 $history_title = get_field('history_title');
 $text_about_history = get_field('text_about_history');
+$img2 = get_field('img2');
 $text_about_competition_title = get_field('text_about_competition_title');
 $text_about_competition = get_field('text_about_competition');
-$img2 = get_field('img2');
+$img3 = get_field('img3');
 $family_title = get_field('family_title');
 $family_text = get_field('family_text');
+$img4 = get_field('img4');
 $join_us_title = get_field('join_us_title');
 $join_us_text = get_field('join_us_text');
-$img3 = get_field('img3');
+$img5 = get_field('img5');
 $contact_us_title = get_field('contact_us_title');
 $contact_us_text = get_field('contact_us_text');
 
@@ -35,19 +37,22 @@ $contact_us_text = get_field('contact_us_text');
 <div class="about-us flex-col" >
 
     <section class="history">
-        <div class="history-text flex-col text-margin text-style">
+        <div class="history-text flex-row text-margin text-style">
             <h3><?php echo esc_html($history_title); ?></h3>
             <p><?php echo esc_html($text_about_history); ?></p>
+        </div>
+        <div class="history-img">
+            <img src="<?php echo esc_url($img2['url']); ?>" alt="about-us-history-img" id="img">
         </div>
     </section>
 
     <section class="competition flex-row">
+        <div class="competition-img">
+            <img src="<?php echo esc_url($img3['url']); ?>" alt="about-us-competition-img" id="img">
+        </div>
         <div class="competition-text flex-col text-margin text-style">
             <h3><?php echo esc_html($text_about_competition_title); ?></h3>
             <p><?php echo esc_html($text_about_competition); ?></p>
-        </div>
-        <div class="competition-img">
-            <img src="<?php echo esc_url($img2['url']); ?>" alt="about-us-competition-img" id="img">
         </div>
     </section>
 
@@ -56,22 +61,28 @@ $contact_us_text = get_field('contact_us_text');
             <h3><?php echo esc_html($family_title); ?></h3>
             <p><?php echo esc_html($family_text); ?></p>
         </div>
+        <div class="family-img">
+            <img src="<?php echo esc_url($img4['url']); ?>" alt="about-us-family-img" id="img">
+        </div>
     </section>
 
     <section class="join-us flex-row">
+        <div class="join-us-img">
+            <img src="<?php echo esc_url($img5['url']); ?>" alt="about-us-join-us-img" id="img">
+        </div>
         <div class="join-us-text flex-col text-margin text-style">
             <h3><?php echo esc_html($join_us_title); ?></h3>
             <p><?php echo esc_html($join_us_text); ?></p>
         </div>
-        <div class="join-us-img">
-            <img src="<?php echo esc_url($img3['url']); ?>" alt="about-us-join-us-img" id="img">
-        </div>
     </section>
 
     <section class="contact-us">
-        <div class="contact-us-text flex-col text-margin text-style">
+        <div class="contact-us-text flex-row text-margin text-style">
             <h3><?php echo esc_html($contact_us_title); ?></h3>
             <p><?php echo esc_html($contact_us_text); ?></p>
+        </div>
+        <div class="contact-us-img">
+            <img src="<?php echo esc_url($img2['url']); ?>" alt="about-us-contact-us-img" id="img">
         </div>
     </section>
 
