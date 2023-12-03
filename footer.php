1f1607154b713993fe80d32d551044c1d66d924a
KianMiridoozini <114469799+KianMiridoozini@users.noreply.github.com>
2023-12-03 12:34:14 +0100

    added links to footer.php, and adjusted footer in homepage.css

--- footer.php
+++ footer.php
@@ -1,10 +1,24 @@
-            </main>
+<?php
+$loop = new WP_Query(array(
+    "post_type" => "footer",
+    "posts_per_page" => 1
+));
+
+while($loop->have_posts()) {
+    $loop->the_post();
+    $instagram = get_field('instagram');
+    $facebook = get_field('facebook');
+}
+wp_reset_postdata(); 
+?>    
+            
+    </main>
             <footer>
                 <div class="some">
-                    <a href="">
+                    <a href="<?php echo ($facebook)?>">
                         <img src="http://cateringbbq.local/wp-content/themes/catering/Catering/assets/facebook.svg" alt="">
                     </a>
-                    <a href="">
+                    <a href="<?php echo ($instagram)?>">
                         <img src="http://cateringbbq.local/wp-content/themes/catering/Catering/assets/instagram.svg" alt="">
                     </a>
                 </div>
@@ -13,6 +27,7 @@
                     <a href="<?php echo get_site_url(); ?>/copyrights">Copyrights</a>
                     <a href="<?php echo get_site_url(); ?>/privacy-policy">Privacy Policy</a>
                 </div>
+                <p>©2023 <span style="font-weight: bolder" >BBQ Catering West-Coast</span>
             </footer>
         <?php wp_footer(); ?>
     </body>
